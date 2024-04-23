5697f128de86a9381f8d580e0b05f8b140f22fba
add_node <ranasahil78922@gmail.com>
2024-04-23 05:48:01 +0530

    first commit

---
+++ app.jsx
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const uri = 'mongodb+srv://ranasahil78922:MhT1KYRyOByM28Pg@crud.lfhcbaq.mongodb.net/data?retryWrites=true&w=majority&appName=Crud';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const Item = mongoose.model('Item', itemSchema, 'item');
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'https://shopiee.hubsyntax.com'
+}));
+
+app.post('/api/items', async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const newItem = new Item({ name, description });
+    await newItem.save();
+    console.log('Item created successfully:', newItem);
+    res.status(201).json({ message: 'Item created successfully', item: newItem });
+  } catch (error) {
+    console.error('Error creating item:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
